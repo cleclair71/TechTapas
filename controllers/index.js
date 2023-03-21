3178fc6ae182a5c459e5f226175803b5010ac8e7
cass <76407605+cleclair71@users.noreply.github.com>
2023-03-20 20:44:02 -0400

    index.js router

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./home_routes.js');
+const dashboardRoutes = require('./dashboard_routes.js');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+    });
+
+module.exports = router;
