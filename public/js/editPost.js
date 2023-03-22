f9897840ec2d7b8f49d379d56a335d7d1ce33b6b
cass <76407605+cleclair71@users.noreply.github.com>
2023-03-22 16:10:25 -0400

    edit post js

---
+++ public/js/editPost.js
@@ -0,0 +1,23 @@
+async function editPost(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('input[name="post-content"]').value;
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    if (title && content) {
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editPost);
