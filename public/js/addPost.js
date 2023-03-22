df2044aa4f804a7299ad015c46f66c8b080807b0
cass <76407605+cleclair71@users.noreply.github.com>
2023-03-22 15:57:15 -0400

    addPost functions

---
+++ public/js/addPost.js
@@ -0,0 +1,19 @@
+async function addPost(event) {
+    event.preventDefault();
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+    if (title && content) {
+        const response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('#add-post-form').addEventListener('submit', addPost);
